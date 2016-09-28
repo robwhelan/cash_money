1c89f4fd75bf13aa85e79474a44af879c6d96644
robwhelan <whelan@gmail.com>
2016-09-28 16:04:36 -0400

    auth

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -2,6 +2,14 @@ class BotsController < ApplicationController
 
   def api_ai_webhook
     puts "hey I just got a webhook"
+    my_hash = JSON.parse('{
+        "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+        "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+        "data": {},
+        "contextOut": [],
+        "source": "Rails app"
+      }')
+    render json: my_hash
   end
 
 end
